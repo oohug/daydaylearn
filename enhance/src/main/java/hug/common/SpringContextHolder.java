0ec9b0193502d87b2449bf51f0462d5b0ec0741d
huchao <chao.hu@freemud.com>
2020-08-31 17:25:59 +0800

    enhance

---
+++ enhance/src/main/java/hug/common/SpringContextHolder.java
@@ -0,0 +1,17 @@
+package hug.common;
+
+import lombok.Getter;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class SpringContextHolder implements ApplicationContextAware {
+
+    @Getter
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringContextHolder.applicationContext = applicationContext;
+    }
+}
